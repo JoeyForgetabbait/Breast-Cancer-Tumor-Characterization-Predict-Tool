ff45683509a4699f59ce45acf54207d517f870f9
JoeyForgetabbait <josephcreanza@Josephs-MacBook-Pro.local>
2024-07-18 18:51:03 -0700

    u2d not working tho

--- cancer_app_html/.ipynb_checkpoints/tumorapp-checkpoint.py
+++ cancer_app_html/.ipynb_checkpoints/tumorapp-checkpoint.py
@@ -1,19 +1,36 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, jsonify, request
+import joblib
 
 app = Flask(__name__)
+
+# Load the pre-trained model from the .pkl file
+model = joblib.load('SVM_Model.pkl')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/runprediction', methods=['POST'])
 def run_prediction():
     input_data = request.json
 
     # Extract input values from the request
-    radius_mean = input_data.get('radius_mean')
-    # Extract other input values here
+    radius = input_data.get('radius')
+    texture = input_data.get('texture')
+    perimeter = input_data.get('perimeter')
+    area = input_data.get('area')
+    smoothness = input_data.get('smoothness')
+    compactness = input_data.get('compactness')
+    concavity = input_data.get('concavity')
+    concave_points = input_data.get('concave_points')
+    symmetry = input_data.get('symmetry')
+    fractal_dimension = input_data.get('fractal_dimension')
 
-    # Perform tumor prediction based on the input data
-    result = "Benign" if float(radius_mean) < 15 else "Malignant"
+    # Make predictions using the loaded model
+    result = model.predict([[radius, texture, perimeter, area, smoothness, compactness, concavity, concave_points, symmetry, fractal_dimension]])
 
-    return jsonify({'result': result})
+    # Return the prediction result
+    return jsonify({'result': result[0]})
 
 if __name__ == '__main__':
     app.run()
